e18f54278d8c31d266b265d501c9f9ea1ed7bd56
Pat Allan <pat@freelancing-gods.com>
2010-07-29 15:39:24 +1000

    Using my personal preferences for super-short git aliases. g = git status; l = git log; d = git diff; gco = git checkout; gcom = git checkout master.

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -1,6 +1,7 @@
 # Aliases
-alias g='git'
-alias gst='git status'
+alias g='git st'
+alias l='git log'
+alias d='git diff'
 alias gl='git pull'
 alias gup='git fetch && git rebase'
 alias gp='git push'
@@ -12,7 +13,8 @@ alias gb='git branch'
 alias gba='git branch -a'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
-
+alias gco='git checkout'
+alias gcom='git checkout master'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
